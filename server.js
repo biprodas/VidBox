593d36119fdb1270372be519232f36b1e18764f4
Biprodas <biprodas.cse@gmail.com>
2019-05-04 00:17:02 +0600

    Building RESTfull APIs using express

--- server.js
+++ server.js
@@ -28,15 +28,9 @@ app.get('/api/courses', (req, res) => {
 
 // create a new course
 app.post('/api/courses', (req, res) => {
-  const schema = {
-    name: Joi.string().min(3).required(),
-    author: Joi.string().min(3).required()
-  }
-  const validation = Joi.validate(req.body, schema);
-  console.log(validation);
-  if(validation.error){
-    return res.status(404).send(validation.error.details[0].message);
-  }
+  // Validation
+  const { error } = validateCourse(req.body);
+  if(error) return res.status(404).send(error.details[0].message);
 
   const course = {
     id: courses.length + 1,
@@ -50,10 +44,51 @@ app.post('/api/courses', (req, res) => {
 // get a course by id
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if(!course) res.status(404).send('The course with the given ID wan not found');
+  if(!course) return res.status(404).send('The course with the given ID wan not found');
   res.send(course);
 });
 
+// update course by id
+app.put('/api/courses/:id', (req, res) => {
+  // Look up the course
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  // if not existing, return 404
+  if(!course) return res.status(404).send('The course with the given ID wan not found');
+
+  // Validation
+  const { error } = validateCourse(req.body);
+  //if invalid, return 400 - Bad request
+  if(error) return res.status(404).send(error.details[0].message);
+
+  // Update course
+  course.name = req.body.name;
+  course.author = req.body.author
+  // Return the updated course
+  res.send(course);
+});
+
+// delete a user by id
+app.delete('/api/courses/:id', (req, res) => {
+  // Look up the course
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  // if not existing, return 404
+  if(!course) return res.status(404).send('The course with the given ID wan not found');
+
+  // Delete
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  // Return the deleted course
+  res.send(course);
+})
+
+validateCourse = (course) => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+    author: Joi.string().min(3).required()
+  }
+  return Joi.validate(course, schema);
+}
 
 // Listening on port
 const port = process.env.PORT || 5000;
